337d7e55c95d17e0f7af8c355349b52dcb14d745
João Victor Garcia <joao.garcia18@fatec.sp.gov.br>
2022-07-30 11:16:57 -0300

    updates on file css

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -14,13 +14,13 @@ const Page = () => {
             <div className="main-content">
                 <header className="head-contents">
                     <h1 className="title">CRUD | E-CommercePlay</h1>
-                    <button className="default" onClick={handleLogout}>SAIR</button>
+                    <button className="default" onClick={handleLogout}>Sair</button>
                 </header>
 
                 <nav className="crud-nav">
+                    <h2 className="nav-title">Lista de produtos</h2>
                     <ul>
                         <li className="crud-list"><button className="default">Novo Produto</button></li>
-                        <li className="crud-list"><button className="default edit">Editar</button></li>
                         <li className="crud-list"><button className="default delete">Excluir</button></li>
                     </ul>
                 </nav>
